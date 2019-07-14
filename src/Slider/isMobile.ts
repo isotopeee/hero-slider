4b3f821d9ad6fa9cb0d843d881251a4f8e3b2ebc
isotopeee <johnmichael.doroy@gmail.com>
2019-07-14 13:38:48 +0800

    fix: error on server-side rendering (GastbyJS)
    Add a check for server-side rendering environments.

--- src/Slider/isMobile.ts
+++ src/Slider/isMobile.ts
@@ -1,5 +1,5 @@
 export const isMobile = (): boolean => {
-  if (/Mobi|Android/i.test(navigator.userAgent)) {
+  if (navigator !== 'undefined' && /Mobi|Android/i.test(navigator.userAgent)) {
     return true
   }
   return false
